a692ff7c41718bf7f7d1d6871e4d1204fde0fec0
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-05-29 00:10:40 -0700

    model tests

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe Donor do
   it { should have_many(:donations) }
   it { should have_many(:residents).through(:donations) }
+  it { should allow_value(19177413160, 9177413160).for (:phone)}
+  it { should_not allow_value(171316).for (:phone)}
+  it { should_not allow_value('pie').for (:phone)}
   it { should allow_value('melanieplaza@gmail.com', 'melanie.plaza@aya.yale.edu').for(:email) }
   it { should_not allow_value('melp', '@', 'apple@stout').for(:email)}
 end
--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -5,6 +5,8 @@ describe Partner do
   it { should allow_value('melanieplaza@gmail.com', 'melanie.plaza@aya.yale.edu').for(:email) }
   it { should_not allow_value('melp', '@', 'apple@stout').for(:email)}
   it { should allow_value(19177413160, 9177413160).for (:phone)}
+  it { should_not allow_value(171316).for (:phone)}
+  it { should_not allow_value('pie').for (:phone)}
   it { should allow_value('melanieplaza.com', 'awesomeapp.herokuapp.com/awesome').for (:website)}
   it { should_not allow_value('applesauce', 'five.', 123, 'h12j45f48s', '.com').for (:website)}
 end
