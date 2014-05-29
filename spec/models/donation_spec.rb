7c52f10e89bb0c8ee7916f043970bc22e191ef8f
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-05-28 17:09:13 -0700

    adding tests

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe Donation do
   it { should validate_numericality_of(:amount).only_integer }
+  it { should belong_to(:donor) }
+  it { should belong_to(:resident) }
 end
--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Donor do
+  it { should have_many(:donations) }
+  it { should have_many(:residents).through(:donations) }
   it { should allow_value('melanieplaza@gmail.com', 'melanie.plaza@aya.yale.edu').for(:email) }
   it { should_not allow_value('melp', '@', 'apple@stout').for(:email)}
 end
