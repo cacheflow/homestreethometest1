bbc3b1f995b8800fd3ae324313f5d893cbab2edb
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-05-30 14:31:41 -0700

    adding controllers

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,13 @@
+class StatusesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
---
+++ spec/views/donation/create.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "donation/create.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
