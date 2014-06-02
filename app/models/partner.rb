d0482d4309f08d9f35b233d523ca91f557085d76
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-02 14:01:10 -0700

    user relationships and cancan integration

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,5 @@
 class Partner < ActiveRecord::Base
+  belongs_to :user
   has_many :residents
   validates_format_of :email, with: /[a-zA-Z0-9]\@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/
   validates_format_of :website, with: /\A(\w|[.])[^\W]\w{1,}\.[a-zA-Z]{2,}/
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :role
+  has_many :partners
+  has_many :donors
+  has_many :residents
 end
