c131776e8cdde51a4be22ac369603d2fc5c296ed
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-02 19:56:49 -0700

    lex better not push to master

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,20 +1,28 @@
 class DonationsController < ApplicationController
 
-  def index
-  	@donations = Donation.all
-  end
   def new
+  	@resident = Resident.find(params[:resident_id])
   	@donation = Donation.new
   end
+
   def create
+  	@resident = Resident.find(params[:resident_id])
     @donation = Donation.new(donation_params)
     if @donation.save
-      redirect_to donations_path
+      redirect_to residents_path
     else
       render 'new'
     end
   end
+
+  def show
+    @resident = Resident.find(params[:resident_id])
+    @donation = Post.find(params[:id])
+  end
+
   def donation_params
   	params.require(:donation).permit(:amount, :donor_id, :resident_id)
   end
+
+  
 end
