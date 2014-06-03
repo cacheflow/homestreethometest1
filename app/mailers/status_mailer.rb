80f86292c1fb54327acf066dfaee6f01461c443c
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-03 11:41:41 -0700

    redirect authorization from cancan

--- app/mailers/status_mailer.rb
+++
@@ -1,8 +0,0 @@
-class StatusMailer < ActionMailer::Base
-  default from: "homestreethomemail@gmail.com"
-
-  def welcome_email(user)
-  	@user = user
-  	mail(to: @user.email, subject: "Thanks for signing up!")
-  end
-end
