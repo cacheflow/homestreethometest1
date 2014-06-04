356b614e949f6afe5a6ecb3c475b6c0f34662424
Melanie Plaza <melanie@Melanies-MacBook-Air.local>
2014-06-04 15:30:58 -0700

    starting serialization and angularification

---
+++ app/assets/javascripts/partners.js
@@ -0,0 +1,30 @@
+var partnerApp = angular.module('partner-app', ['ngResource']).config(
+    ['$httpProvider', function($httpProvider) {
+    var authToken = angular.element("meta[name=\"csrf-token\"]").attr("content");
+    var defaults = $httpProvider.defaults.headers;
+
+    defaults.common["X-CSRF-TOKEN"] = authToken;
+    defaults.patch = defaults.patch || {};
+    defaults.patch['Content-Type'] = 'application/json';
+    defaults.common['Accept'] = 'application/json';
+}]);
+
+partnerApp.factory('Partner', ['$resource', function($resource) {
+  return $resource('/partners/:id',
+     {id: '@id'},
+     {update: { method: 'PATCH'}});
+}]);
+
+partnerApp.controller('PartnerCtrl', ['$scope', 'Partner', function($scope, Partner) {
+    $scope.partners= [];
+
+
+    Partner.query(function(partners) {
+      $scope.partners = partners;
+    });
+
+$scope.showPartner = function(partner){
+      partner.details = true;
+    }
+
+   }])
--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,5 +1,8 @@
 class PartnersController < ApplicationController
   load_and_authorize_resource
+
+  respond_to :json, :html
+
   def new
     @partner = Partner.new
   end
@@ -7,10 +10,16 @@ class PartnersController < ApplicationController
   def create
     @partner = Partner.new(partner_params)
     if @partner.save
-      UserMailer.welcome_email(@partner).deliver
-      redirect_to partners_path
+      respond_to do |format|
+        format.html {UserMailer.welcome_email(@partner).deliver
+          redirect_to partners_path}
+        format.json {render json: @partner, status: :created}
+      end
     else
-      render 'new'
+      respond_to do |format|
+        format.html { render 'new' }
+        format.json { render json: @partner.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -23,19 +32,27 @@ class PartnersController < ApplicationController
     @partner = Partner.find(params[:id])
     @residents = @partner.residents
     if @partner.update(partner_params)
-      redirect_to partners_path
-    else 
-      render 'edit'
+      respond_to do |format|
+        format.html { redirect_to partners_path }
+        format.json { render nothing: true, status: :no_content }
+      end
+   else
+      respond_to do |format|
+        format.html { render 'edit' }
+        format.json { render json: @partner.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def index
     @partners = Partner.all
+    respond_with @partners
   end
 
   def show
     @partner = Partner.find(params[:id])
     @residents = @partner.residents
+    respond_with @partner
   end
   def partner_params
     params.require(:partner).permit(:name, :org, :address, :phone, :email, :website, :user_id, :resident_id)
